8daa518e367aec7246baa4cac37517a818a990be
CG MOBILE PARIS <108140594+cg-mobile-paris@users.noreply.github.com>
2022-07-08 17:45:25 +0200

    Update product_template.py

--- cgm-addons/cgm_sale/models/product_template.py
+++ cgm-addons/cgm_sale/models/product_template.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+
 from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    form_id = fields.Many2one('product.form', 'Form')
+    form_id = fields.Many2one('product.form', 'Product Form')
