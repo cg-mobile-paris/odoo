15793ef79dc0cae25b00041495c3e9627a0d62d4
Omar OUHARI <omar.ouhari@gmail.com>
2022-07-11 09:58:54 +0200

    [FIX] : code review

--- cgm-addons/cgm_sale/__manifest__.py
+++ cgm-addons/cgm_sale/__manifest__.py
@@ -17,14 +17,14 @@
     'data': [
         # data
         'data/product_form_data.xml',
-        'data/product_licence_data.xml',
         'data/product_material_data.xml',
+        'data/product_licence_data.xml',
         # security
         'security/ir.model.access.csv',
         # views
         'views/product_form_views.xml',
-        views/product_licence_views.xml,
         'views/product_material_views.xml',
+        'views/product_licence_views.xml',
         'views/product_template_views.xml',
         'views/ir_ui_menu.xml',
     ],
--- cgm-addons/cgm_sale/models/product_template.py
+++ cgm-addons/cgm_sale/models/product_template.py
@@ -6,7 +6,6 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-
     form_id = fields.Many2one('product.form', 'Product Form')
     licence_id = fields.Many2one('product.licence', 'Product Licence')
     material_id = fields.Many2one('product.material', 'Product Material')
