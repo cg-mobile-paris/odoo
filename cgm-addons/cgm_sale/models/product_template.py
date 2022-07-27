a63912f5edccd2208e266730a92bd25edecd120e
Omar OUHARI <omar.ouhari@gmail.com>
2022-07-27 18:15:07 +0200

    [FIX]: improve state need flux

--- cgm-addons/cgm_sale/models/product_template.py
+++ cgm-addons/cgm_sale/models/product_template.py
@@ -9,7 +9,7 @@ class ProductTemplate(models.Model):
 
     form_id = fields.Many2one('product.form', 'Product Form')
     licence_id = fields.Many2one('product.licence', 'Product Licence')
-    material_id = fields.Many2one('product.material', 'Product Material')    
+    material_id = fields.Many2one('product.material', 'Product Material')
     collection_id = fields.Many2one('product.collection', 'Product Collection')
     device_id = fields.Many2one('product.device', 'Product Device')
     brand_id = fields.Many2one('product.brand', 'Product Brand')
