75c2fab3cb074393386eb355f12a6fbfad043340
Rado Niaina RAKOTONIRINA <r.radoniaina11@gmail.com>
2022-07-28 14:53:10 +0300

    [IMPL] #CGM-377 : uniq EAN

--- cgm-addons/cgm_sale/models/product_template.py
+++ cgm-addons/cgm_sale/models/product_template.py
@@ -22,3 +22,9 @@ class ProductTemplate(models.Model):
         for record in self:
             if self.search([('default_code', '!=', False), ('default_code', '=', record.default_code), ('id', '!=', record.id)], limit=1):
                 raise ValidationError(_('An item with the same SKU Code already exists in the system.'))
+
+    @api.constrains('barcode')
+    def _check_unique_barcode(self):
+        for record in self:
+            if self.search([('barcode', '!=', False), ('barcode', '=', record.barcode), ('id', '!=', record.id)], limit=1):
+                raise ValidationError(_('An item with the same EAN Code already exists in the system.'))
