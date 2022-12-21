2313b77d6fe9634d6b86d64a1d47d602e460ca15
etechsam <etechsnoom@gmail.com>
2022-12-21 16:29:43 +0300

    [FIX] #CGMTMA-11 add bank account in sale order an invoice pdf

--- cgm-addons/cgm_sale/models/sale_order.py
+++ cgm-addons/cgm_sale/models/sale_order.py
@@ -11,6 +11,8 @@ class SaleOrder(models.Model):
                                           default=lambda self: self.env.company.currency_id, copy=False)
     amount_total_in_currency = fields.Monetary('Total', readonly=True, currency_field='company_currency_id', copy=False)
     total_qty = fields.Float('Total Qty', compute='_compute_total_qty', digits='Product Unit of Measure', store=True)
+    bank_id = fields.Many2one('res.partner.bank', string='Bank account')
+    bank_partner_id = fields.Many2one('res.partner', string='Bank partner', related='company_id.partner_id')
 
     @api.depends('order_line', 'order_line.product_uom_qty')
     def _compute_total_qty(self):
@@ -62,10 +64,18 @@ class SaleOrder(models.Model):
         if not (self.company_id.update_currency_rates()):
             raise UserError(_('Unable to connect to the online exchange rate platform. '
                               'The web service may be temporary down. Please try again in a moment.'))
-        return self.company_currency_id._convert(self.amount_total, self.currency_id, self.company_id, fields.Date.today())
+        return self.company_currency_id._convert(self.amount_total, self.currency_id, self.company_id,
+                                                 fields.Date.today())
 
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for order in self:
             order.compute_amount_total_in_currency()
+        return res
+
+    def _prepare_invoice(self):
+        self.ensure_one()
+        res = super(SaleOrder, self)._prepare_invoice()
+        res['partner_bank_id'] = self.bank_id.id or self.company_id.partner_id.bank_ids.filtered(
+            lambda bank: bank.company_id.id in (self.company_id.id, False))[:1].id
         return res
