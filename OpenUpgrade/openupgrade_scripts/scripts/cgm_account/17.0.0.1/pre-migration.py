1e6f7ae22b84074c426437d38091ce07cbdc5bc0
Lilian Olivier <lilian.olivier@somniplanet.com>
2024-02-28 14:46:03 +0100

    [ADD] pre post migration script

--- OpenUpgrade/openupgrade_scripts/scripts/cgm_account/17.0.0.1/pre-migration.py
+++ OpenUpgrade/openupgrade_scripts/scripts/cgm_account/17.0.0.1/pre-migration.py
@@ -10,10 +10,14 @@ _logger = logging.getLogger(__name__)
 # from modules.cgmobileparis.OpenUpgrade.openupgrade_scripts.apriori import ...
 # from ...OpenUpgrade.openupgrade_scripts.apriori import merged_modules, renamed_modules, renamed_models, merged_models, renamed_tables
 
-
+print('ooooooooooooooooooooooo')
 
 def delete_views(env):
     view_list = [
+        'base.module_arkeup_background_image',
+        'base.module_purchase_reception_status',
+        'base.module_sale_delivery_state',
+        'base.module_smile_api_rest',
         'view_move_line_tree',
         'action_account_move_line',
         'account_move_view_tree_inherit',
@@ -21,13 +25,26 @@ def delete_views(env):
         'report_saleorder_document_inherit',
         'res_bank_form_inherit',
         'cg_mobile_web_layout',
+        'sale_delivery_state.view_order_tree_inherit_delivery_state',
+        'sale_delivery_state.view_quotation_tree_inherit_delivery_state',
+        'sale_delivery_state.view_order_form_inherit_delivery_state',
+        'purchase_reception_status.purchase_order_form',
+        'purchase_reception_status.view_purchase_order_filter',
+        'purchase_reception_status.purchase_order_tree',
+        'purchase_reception_status.purchase_order_view_tree',
 
     ]
-    print('delete_views !!!')
+    print('openupgrade: delete_views')
 
     openupgrade.delete_records_safely_by_xml_id(env, view_list)
 
+# def delete_module(env):
+#     openupgrade.logged_query("""DELETE FROM ir_module_module
+# WHERE name=""
+# RETURNING (select_list | *);""")
+
 def rename_user(env):
+    print('rename user')
     get_data_query = """UPDATE res_users SET password = 'admin' WHERE login = 'admin@cg-mobile.com';"""
     env.cr.execute(get_data_query)
     get_data_query = """UPDATE res_users SET login = 'admin' WHERE login = 'admin@cg-mobile.com';"""
@@ -35,6 +52,7 @@ def rename_user(env):
 
 @openupgrade.migrate()
 def migrate(env, version):
+    print('migrate')
     """
     Don't request an env for the base pre-migration as flushing the env in
     odoo/modules/registry.py will break on the 'base' module not yet having
