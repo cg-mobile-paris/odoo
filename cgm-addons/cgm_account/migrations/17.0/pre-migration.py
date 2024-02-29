df9a6a26f4b3ab4228895030cfb38979c75e990f
Lilian Olivier <lilian.olivier@somniplanet.com>
2024-02-29 16:46:51 +0100

    [MIG]

--- cgm-addons/cgm_account/migrations/17.0/pre-migration.py
+++ cgm-addons/cgm_account/migrations/17.0/pre-migration.py
@@ -6,7 +6,7 @@ def delete_views(env):
         'base.module_arkeup_background_image',
         'base.module_purchase_reception_status',
         'base.module_sale_delivery_state',
-        'base.module_smile_api_rest',
+        # 'base.module_smile_api_rest',
         'cgm_account.view_move_line_tree',
         'cgm_account.action_account_move_line',
         'cgm_account.account_move_view_tree_inherit',
@@ -21,30 +21,33 @@ def delete_views(env):
         'purchase_reception_status.view_purchase_order_filter',
         'purchase_reception_status.purchase_order_tree',
         'purchase_reception_status.purchase_order_view_tree',
-        # 'smile_api_rest.model_api_rest_version',
-        # 'smile_api_rest.model_api_rest_field',
-        # 'smile_api_rest.model_api_rest_function_parameter',
-        # 'smile_api_rest.model_api_rest_log',
-        # 'smile_api_rest.model_api_rest_path',
-        # 'smile_api_rest.model_api_rest_tag',
-        # 'smile_api_rest.model_api_rest_version',
-        # 'smile_api_rest.menu_api_rest',
-        # 'smile_api_rest.menu_action_api_rest_version',
-        # 'smile_api_rest.menu_action_api_rest_path',
-        # 'smile_api_rest.menu_api_rest_configuration',
-        # 'smile_api_rest.menu_action_api_rest_tag',
+        'smile_api_rest.model_api_rest_field',
+        'smile_api_rest.model_api_rest_function_parameter',
+        'smile_api_rest.model_api_rest_log',
+        'smile_api_rest.model_api_rest_path',
+        'smile_api_rest.model_api_rest_tag',
+        'smile_api_rest.model_api_rest_version',
+        'smile_api_rest.menu_api_rest',
+        'smile_api_rest.menu_action_api_rest_version',
+        'smile_api_rest.menu_action_api_rest_path',
+        'smile_api_rest.menu_api_rest_configuration',
+        'smile_api_rest.menu_action_api_rest_tag',
     ]
-    print('openupgrade: delete_views')
+
 
     for v in view_list:
+        print('odoo: delete_views', v)
         util.records.remove_record(env, v)
 
 def manage_modules(env):
-    util.uninstall_module(env, 'smile_api_rest')
-    util.remove_module(env, 'smile_api_rest')
+    modules = ['smile_api_rest', 'api_rest', 's2u_oauth2', 'sale_delivery_state', 'purchase_reception_status']
+    for m in modules:
+        print('odoo: manage_modules', m)
+        util.uninstall_module(env, m)
+        util.remove_module(env, m)
 
 def migrate(cr, version):
     print('MIGRATE')
+    manage_modules(cr)
     delete_views(cr)
-    # manage_modules(cr)
 
